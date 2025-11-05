ebf7d29658169bef1fc83dadc6bc303dfda236e0
Tancrède Lamort de Gail <tancredelamortdegail@gmail.com>
2025-11-05 18:26:06 +0100

    Added gitignore & updated task/robot type logic with enums - gitignore to ignore build files (+ removed current build files) - added enum types for task and robot type to message.h - updated task and robot type logic to use the enums - my c/c++ code formatter also kinda "fixed" the formatting in the epuck and auct_super files (uses Google's formatting style)

--- controllers/auct_super/message.h
+++ controllers/auct_super/message.h
@@ -1,34 +1,46 @@
 #include <stdint.h>
 
-// A message sent from the supervisor to the robot
+// TASK_TYPE_A message sent from the supervisor to the robot
 typedef enum {
-  MSG_EVENT_INVALID = 0, 
-  MSG_EVENT_GPS_ONLY,
-  MSG_EVENT_NEW,
-  MSG_EVENT_WON,
-  MSG_EVENT_DONE,
-  MSG_QUIT
+    MSG_EVENT_INVALID = 0,
+    MSG_EVENT_GPS_ONLY,
+    MSG_EVENT_NEW,
+    MSG_EVENT_WON,
+    MSG_EVENT_DONE,
+    MSG_QUIT
 } message_event_state_t;
 
+typedef enum {
+    TASK_TYPE_A = 'A',
+    TASK_TYPE_B = 'B',
+    TASK_TYPE_INVALID = 0
+} task_type_t;
+
+// Also add a robot specialization type
+typedef enum {
+    ROBOT_SPEC_A = 'A',  // Specialized for task A
+    ROBOT_SPEC_B = 'B',  // Specialized for task B
+} robot_spec_t;
+
 typedef struct {
-  uint16_t robot_id; // id of the intended receiver
-  double robot_x;
-  double robot_y;
-  double heading; // robot heading
-  message_event_state_t event_state;
-  // [[ valid if event_state > 0: 
-  uint16_t event_id;
-  uint16_t event_type;
-  double event_x;
-  double event_y;
-  // ]]
-  int event_index;
+    uint16_t robot_id;  // id of the intended receiver
+    double robot_x;
+    double robot_y;
+    double heading;  // robot heading
+    message_event_state_t event_state;
+    // [[ valid if event_state > 0:
+    uint16_t event_id;
+    task_type_t task_type;
+    double event_x;
+    double event_y;
+    // ]]
+    int event_index;
 } message_t;
 
 // A message sent from a robot to the supervisor
 typedef struct {
-  uint16_t robot_id; // id of the sender
-  uint16_t event_id; // id of the event the bid is for
-  double value; // value of the bid (estimated distance)
-  int event_index;
+    uint16_t robot_id;  // id of the sender
+    uint16_t event_id;  // id of the event the bid is for
+    double value;       // value of the bid (estimated distance)
+    int event_index;
 } bid_t;
